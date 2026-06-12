be2e6e87db0fd081123cdbb2febf2f3830cbf3a2
moustillon <aryan.bhotey@epitech.eu>
2026-06-12 15:15:20 +0200

    fix: publicPath added

--- frontend/build.ts
+++ frontend/build.ts
@@ -139,6 +139,7 @@ const result = await Bun.build({
   minify: true,
   target: "browser",
   sourcemap: "linked",
+  publicPath: "/",
   define: {
     "process.env.NODE_ENV": JSON.stringify("production"),
     "__API_URL__": JSON.stringify(process.env.API_URL ?? "http://localhost:8080"),
